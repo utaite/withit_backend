bbdad445754d45548585f2207285e7edfd91d99c
mingi123 <wjd4813@naver.com>
2024-04-10 02:42:16 +0900

    Init commit

---
+++ src/main/kotlin/com/example/withit/WithitApplication.kt
@@ -0,0 +1,11 @@
+package com.example.withit
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class WithitApplication
+
+fun main(args: Array<String>) {
+	runApplication<WithitApplication>(*args)
+}
---
+++ src/test/kotlin/com/example/withit/WithitApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.withit
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WithitApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
